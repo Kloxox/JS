98da8672a1c3e47b9a82c7626b0a99f665e06c5f
Kloxox <90182051+Kloxox@users.noreply.github.com>
2023-03-09 23:59:34 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Exercice 10</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.js">
+</head>
+<body>
+    <div id ="Methode1 class ="divD">Clic moi dessus</div>
+    <div id ="Methode2 class ="divD">Survole moi dessus</div>
+    <div id ="Methode3 class ="DivD" tabindex="0">Appui au clavier</div>
+</body>
+<script src ="main.js"></script>
+</html>
